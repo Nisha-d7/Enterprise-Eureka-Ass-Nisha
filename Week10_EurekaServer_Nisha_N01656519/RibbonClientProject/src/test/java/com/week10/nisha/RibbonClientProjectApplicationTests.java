96917d9491cf5522cff7706373e80f346acc1f41
Nisha <nishars1854@gmail.com>
2025-07-14 21:42:53 -0400

    Three Projects

---
+++ Week10_EurekaServer_Nisha_N01656519/RibbonClientProject/src/test/java/com/week10/nisha/RibbonClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week10.nisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonClientProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
